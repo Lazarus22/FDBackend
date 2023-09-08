329aac088ec9938d5b29cbb7241aa1b7c52a139c
Moss <moss.makes@gmail.com>
2023-09-08 17:05:51 -0400

    refactor

--- cypherQueries/queries.cypher
+++ cypherQueries/queries.cypher
@@ -1,7 +1,3 @@
-// GetRecommendationsQuery
-MATCH (i1)-[r:pairs_with]->(i2)
-WHERE i1.name = $flavor OR r.property = $flavor
-RETURN i2.name AS recommendation, r.value AS strength
 // GetAllFlavorsQuery
 MATCH (n:Flavor)
 RETURN n.name AS flavorName
--- internal/recommendations/recommend.go
+++ internal/recommendations/recommend.go
@@ -6,7 +6,6 @@ import (
 	"encoding/json"
 	"github.com/neo4j/neo4j-go-driver/v5/neo4j"
 	"net/http"
-	"fmt"
 )
 
 type Pairing struct {
@@ -20,111 +19,53 @@ type RecommendationsResponse struct {
 }
 
 func NewHandler(driver neo4j.DriverWithContext) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request) {
-			flavor := r.URL.Query().Get("flavor")
+	return func(w http.ResponseWriter, r *http.Request) {
+		ctx := context.Background()
 
-			fmt.Printf("Received flavor: %s\n", flavor)
+		flavor := r.URL.Query().Get("flavor")
 
-			// Create a session
-			session := driver.NewSession(neo4j.SessionConfig{})
-			defer session.Close()
+		session := driver.NewSession(ctx, neo4j.SessionConfig{})
+		defer session.Close(ctx)
 
-			// Fetching the Cypher query from your map
-			query, err := cypherQueries.GetRecommendationsQuery("GetRecommendationsQuery")
-			if err != nil {
-				fmt.Printf("Error fetching query: %v\n", err)
-				w.WriteHeader(http.StatusInternalServerError)
-				return
-			}
-
-		var recommendations []Pairing
-		result, err := session.Run(query, map[string]interface{}{"flavor": flavor})
-
+		query, err := cypherQueries.GetRecommendationsQuery("GetAllFlavorsQuery")
 		if err != nil {
-			fmt.Printf("Error running query: %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
-		for result.Next() {
+		params := map[string]interface{}{"flavor": flavor}
+		result, err := session.Run(ctx, query, params)
+		if err != nil {
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		var recommendations []Pairing
+		for result.Next(ctx) {
 			record := result.Record()
-			name, _ := record.Get("recommendation")
-			strengthMap, _ := record.Get("strength")
+			name, ok := record.Get("flavorName")
+			if !ok {
+				http.Error(w, "Internal server error", http.StatusInternalServerError)
+				return
+			}
 
-			if strengthData, ok := strengthMap.(map[string]interface{}); ok {
-				strength := strengthData["low"].(int64)
-
-				if nameStr, ok := name.(string); ok {
-					recommendations = append(recommendations, Pairing{Name: nameStr, Strength: strength})
-				}
+			if nameStr, ok := name.(string); ok {
+				recommendations = append(recommendations, Pairing{Name: nameStr, Strength: 0}) // Here Strength is set to 0 as a placeholder.
 			}
 		}
 
 		if err = result.Err(); err != nil {
-			fmt.Printf("Error iterating through query results: %v\n", err)
-			w.WriteHeader(http.StatusInternalServerError)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
 			return
 		}
 
-		fmt.Printf("Final recommendations: %v\n", recommendations)
-
-		response := RecommendationsResponse{
+		resp := RecommendationsResponse{
 			Flavor:          flavor,
 			Recommendations: recommendations,
 		}
 
 		w.Header().Set("Content-Type", "application/json")
-		json.NewEncoder(w).Encode(response)
+		json.NewEncoder(w).Encode(resp)
 	}
 }
 
-
-func getRecommendations(flavor string, driver neo4j.DriverWithContext, query string) ([]Pairing, error) {
-	ctx := context.Background()
-	session := driver.NewSession(ctx, neo4j.SessionConfig{DatabaseName: "neo4j"})
-	defer session.Close(ctx)
-
-	var recommendations []Pairing
-
-	tx, err := session.BeginTransaction(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	params := map[string]interface{}{"flavor": flavor}
-
-	result, err := tx.Run(ctx, query, params)
-	if err != nil {
-		tx.Rollback(ctx)
-		return nil, err
-	}
-
-	for result.Next(ctx) {
-    record := result.Record()
-    name, _ := record.Get("recommendation")
-    strengthMap, _ := record.Get("strength")
-
-    if strengthData, ok := strengthMap.(map[string]interface{}); ok {
-        low, _ := strengthData["low"].(int64)
-        high, _ := strengthData["high"].(int64)
-        strength := low + (high << 32)
-        
-        if nameStr, ok := name.(string); ok {
-            recommendations = append(recommendations, Pairing{Name: nameStr, Strength: strength})
-        }
-    }
-}
-
-
-	if err = result.Err(); err != nil {
-		tx.Rollback(ctx)
-		return nil, err
-	}
-
-	err = tx.Commit(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	return recommendations, nil
-}
